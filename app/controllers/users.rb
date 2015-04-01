82c9201ebc479ba0a710309f021266ca406c3b25
Hoa Nguyen <hoathenguyen85@gmail.com>
2015-04-01 10:35:00 -0500

    Catch error and redirect if password not same with confirm password 	modified:   app/controllers/users.rb 	modified:   app/views/users/new.erb

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,3 +1,9 @@
 get '/users/new' do
   erb :'users/new'
 end
+
+post '/users/new' do
+  redirect to('/users/new?error=Password%20did%20not%20match')  unless params[:password] == params[:confirm_password]
+
+#  Users.create()
+end
