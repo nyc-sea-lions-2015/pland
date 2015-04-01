09f920b2e12c9ee79f709b5af94d451837b6cb21
Hoa Nguyen <hoathenguyen85@gmail.com>
2015-04-01 08:56:28 -0500

    Add relationship between event and user for the creator_id 	modified:   app/models/event.rb 	modified:   app/models/user.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ActiveRecord::Base
   belongs_to :event_type
+  belongs_to :creator, class_name: 'User'
   has_many :daily_events
   has_many :day_schedules, through: :daily_events
 end
