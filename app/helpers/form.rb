870d554d085380c5781e329849e177ff100af844
Hoa Nguyen <hoathenguyen85@gmail.com>
2015-04-01 11:27:11 -0500

    Play with form stuff moving on, edit form finished, just not loading defaults yet 	modified:   app/helpers/form.rb 	modified:   app/views/users/_form.erb

--- app/helpers/form.rb
+++ app/helpers/form.rb
@@ -1,6 +1,8 @@
-def input_form(type, name)
+def input_form(type, name, args={})
   name_for_form = name.split('_').map{|word| word.capitalize}.join(' ')
-  "<label for='#{name}'>#{name_for_form}:</label>
-   <input type='text' placeholder='#{name_for_form}' name ='#{name}'>"
+  html_value = args.has_key?(:value) ? "value='#{args.fetch(:value)}'" : ''
+  html_id = args.has_key?(:id) ? "id='#{args.fetch(:id)}'" : ''
 
+  "<label for='#{name}'>#{name_for_form}:</label>
+   <input type='#{type}' placeholder='#{name_for_form}' name ='#{name}' #{html_value} #{html_id}>"
 end
