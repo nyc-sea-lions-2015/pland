811d84dbdd275fecda16c4ce9a234a29cd2dd7e4
Hoa Nguyen <hoathenguyen85@gmail.com>
2015-04-01 10:49:32 -0500

    Create new user

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -2,8 +2,23 @@ get '/users/new' do
   erb :'users/new'
 end
 
+get '/users/:id' do
+
+end
+
 post '/users/new' do
   redirect to('/users/new?error=Password%20did%20not%20match')  unless params[:password] == params[:confirm_password]
 
-#  Users.create()
+  new_user = User.new(first_name: params[:first_name],
+                      last_name:  params[:last_name],
+                      email:      params[:email],
+                      dob:        DateTime.parse(params[:dob]),
+                      password:   params[:password])
+
+  if new_user.save
+    redirect "/users/#{new_user.id}"
+  else
+    [402, 'Something went wrong']
+  end
 end
+
