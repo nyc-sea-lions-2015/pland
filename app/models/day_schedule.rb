27bb84e992f68783ffe8cba1b5efade851ad429a
Hoa Nguyen <hoathenguyen85@gmail.com>
2015-04-01 09:56:23 -0500

    Made uniqueness in day_schedule model for user_id and date 	modified:   app/models/day_schedule.rb

--- app/models/day_schedule.rb
+++ app/models/day_schedule.rb
@@ -2,4 +2,6 @@ class DaySchedule < ActiveRecord::Base
   belongs_to :user
   has_many :daily_events
   has_many :events, through: :daily_events
+
+  validates_uniqueness_of :user_id, scope: :day
 end
