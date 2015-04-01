f4079c5e25da3c39d7812e44172403b252cbdcf7
Hoa Nguyen <hoathenguyen85@gmail.com>
2015-04-01 10:21:36 -0500

    Add new user form and did js for getting todays date in HTML string 	new file:   app/controllers/users.rb 	new file:   app/views/users/new.erb 	modified:   public/js/application.js

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,13 @@
+Date.prototype.toDateInputValue = (function() {
+  var local = new Date();
+  return local.toJSON().slice(0,10);
+});
+
 $(document).ready(function() {
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('#dob').val(new Date().toDateInputValue());
 });
